47aa4b7cf4e77ab9a871dcb7b31c4a3cf66501d8
BelikovaLyuba <lu-lulyuba@yandex.ru>
2024-12-09 18:11:43 +0300

    Настройка окружения, подключение к API AccuWeather, получение данных о погоде

---
+++ main.py
@@ -0,0 +1,39 @@
+import requests
+import Flask
+from rich import print
+
+api_url = 'https://dataservice.accuweather.com/'
+loc_url = '/locations/v1/cities/geoposition/search'
+api_key = 'UGH2Z3Su53KsHjpz6PT6wcKd5ueF3b77'
+
+latitude = input()
+longitude = input()
+
+loc_key = requests.get(api_url + loc_url,
+                       params = {
+                           'q': f'{latitude},{longitude}',
+                           'apikey': api_key
+                       }).json()['Key']
+
+w_url = f'/currentconditions/v1/{loc_key}'
+r_w = requests.get(api_url + w_url, params={'details': 'true',
+                                            'apikey': api_key}).json()
+
+rain_url = f'/forecasts/v1/daily/1day/{loc_key}'
+r_rain = requests.get(api_url + rain_url, params={'details': 'true',
+                                               'apikey': api_key}).json()
+print(r_rain)
+
+temp = r_w[0]['Temperature']['Metric']['Value']
+print('Температура:', temp, 'C')
+
+humidity = r_w[0]['RelativeHumidity']
+print('Влажность:', humidity, '%')
+
+wind = r_w[0]['Wind']['Speed']['Metric']['Value']
+print('Скорость ветра:', wind, 'km/h')
+
+rain = r_rain['DailyForecasts'][0]['Day']['RainProbability']
+print('Вероятность дождя:', rain)
+
+
